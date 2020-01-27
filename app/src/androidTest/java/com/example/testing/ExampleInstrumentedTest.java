60290bc0b30e4593ccfeb59eed4649753e72fcb0
shahi1 <49000299+shahi1@users.noreply.github.com>
2020-01-27 11:49:24 +0545

    Instrumental testing initial

--- app/src/androidTest/java/com/example/testing/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/testing/ExampleInstrumentedTest.java
@@ -4,10 +4,18 @@ import android.content.Context;
 
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.*;
 
 /**
@@ -17,11 +25,29 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> testRule = new ActivityTestRule<>(MainActivity.class);
+    public String result = "10";
+
     @Test
+    public void TestAddition() {
+        onView(withId(R.id.etfirst)).perform(typeText("6"));
+
+        onView(withId(R.id.etsecond)).perform(typeText("4"));
+/*
     public void useAppContext() {
-        // Context of the app under test.
+         Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
         assertEquals("com.example.testing", appContext.getPackageName());
+        // }*/
+
+        //Button clicked
+
+        onView(withId(R.id.btnSum)).perform(click());
+
+        //onView(withId(R.id.tvResult)).check(matches(withText("Result is :22.0")));
     }
+
 }
--- app/src/test/java/com/example/testing/MyTest.java
+++ app/src/test/java/com/example/testing/MyTest.java
@@ -1,14 +1,22 @@
 package com.example.testing;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import static junit.framework.TestCase.assertEquals;
 
 public class MyTest {
+
+    Arithemetic arithemetic=null;
+    @Before
+    public void setUp()
+    {
+        arithemetic =new Arithemetic();
+    }
     @Test
     public void testAddition()
     {
-        Arithemetic arithemetic=new Arithemetic();
         float expectedResult=10;
         float actualResult=arithemetic.add(4,6);
         assertEquals(expectedResult,actualResult);
@@ -16,18 +24,21 @@ public class MyTest {
     @Test
     public void testSubtraction()
     {
-        Arithemetic arithemetic=new Arithemetic();
-        float expectedResult=2;
-        float actualResult=arithemetic.add(6,4);
+        float expectedResult=10;
+        float actualResult=arithemetic.subtract(20,10);
         assertEquals(expectedResult,actualResult);
 
     }@Test
     public void testDivision()
     {
-        Arithemetic arithemetic=new Arithemetic();
         float expectedResult=2;
-        float actualResult=arithemetic.add(12,6);
+        float actualResult=arithemetic.divide(12,6);
         assertEquals(expectedResult,actualResult);
+    }
+    @After
+    public void tearDown()
+    {
+        arithemetic=null;
     }
 
 }
